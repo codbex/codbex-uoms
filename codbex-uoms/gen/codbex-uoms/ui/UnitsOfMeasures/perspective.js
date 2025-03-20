2fde26a48dac5c85f7e64852bc6d978065ec8572
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-03-20 11:10:23 +0200

    refactor model: - add roles to entities - add roles to models

--- codbex-uoms/gen/codbex-uoms/ui/UnitsOfMeasures/perspective.js
+++ codbex-uoms/gen/codbex-uoms/ui/UnitsOfMeasures/perspective.js
@@ -7,8 +7,8 @@ const perspectiveData = {
 	id: 'UnitsOfMeasures',
 	name: 'Units of Measures',
 	link: '/services/web/codbex-uoms/gen/codbex-uoms/ui/UnitsOfMeasures/index.html',
-	order: 980,
-	icon: '/services/web/resources/unicons/arrows-resize-v.svg',
+	order: '980',
+	icon: '/services/web/resources/unicons/arrows-resize-v.svg'
 };
 
 if (typeof exports !== 'undefined') {
