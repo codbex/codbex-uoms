d01fce32940a259eee36e7dd62be0163ba062a71
StanGenchev <82379274+StanZGenchev@users.noreply.github.com>
2025-04-15 14:33:08 +0300

    #4721 - Migrate old templates to use the new UI (#24)

---
+++ codbex-uoms/gen/codbex-uoms/ui/Settings/Dimension/dialog-window/controller.js
@@ -0,0 +1,86 @@
+angular.module('page', ['blimpKit', 'platformView', 'EntityService'])
+	.config(['EntityServiceProvider', (EntityServiceProvider) => {
+		EntityServiceProvider.baseUrl = '/services/ts/codbex-uoms/gen/codbex-uoms/api/Settings/DimensionService.ts';
+	}])
+	.controller('PageController', ($scope, $http, ViewParameters, EntityService) => {
+		const Dialogs = new DialogHub();
+		$scope.entity = {};
+		$scope.forms = {
+			details: {},
+		};
+		$scope.formHeaders = {
+			select: 'Dimension Details',
+			create: 'Create Dimension',
+			update: 'Update Dimension'
+		};
+		$scope.action = 'select';
+
+		let params = ViewParameters.get();
+		if (Object.keys(params).length) {
+			$scope.action = params.action;
+			$scope.entity = params.entity;
+			$scope.selectedMainEntityKey = params.selectedMainEntityKey;
+			$scope.selectedMainEntityId = params.selectedMainEntityId;
+		}
+
+		$scope.create = () => {
+			let entity = $scope.entity;
+			entity[$scope.selectedMainEntityKey] = $scope.selectedMainEntityId;
+			EntityService.create(entity).then((response) => {
+				Dialogs.postMessage({ topic: 'codbex-uoms.Settings.Dimension.entityCreated', data: response.data });
+				Dialogs.showAlert({
+					title: 'Dimension',
+					message: 'Dimension successfully created',
+					type: AlertTypes.Success
+				});
+				$scope.cancel();
+			}, (error) => {
+				const message = error.data ? error.data.message : '';
+				$scope.$evalAsync(() => {
+					$scope.errorMessage = `Unable to create Dimension: '${message}'`;
+				});
+				console.error('EntityService:', error);
+			});
+		};
+
+		$scope.update = () => {
+			let id = $scope.entity.Id;
+			let entity = $scope.entity;
+			entity[$scope.selectedMainEntityKey] = $scope.selectedMainEntityId;
+			EntityService.update(id, entity).then((response) => {
+				Dialogs.postMessage({ topic: 'codbex-uoms.Settings.Dimension.entityUpdated', data: response.data });
+				Dialogs.showAlert({
+					title: 'Dimension',
+					message: 'Dimension successfully updated',
+					type: AlertTypes.Success
+				});
+				$scope.cancel();
+			}, (error) => {
+				const message = error.data ? error.data.message : '';
+				$scope.$evalAsync(() => {
+					$scope.errorMessage = `Unable to update Dimension: '${message}'`;
+				});
+				console.error('EntityService:', error);
+			});
+		};
+
+
+		$scope.alert = (message) => {
+			if (message) Dialogs.showAlert({
+				title: 'Description',
+				message: message,
+				type: AlertTypes.Information,
+				preformatted: true,
+			});
+		};
+
+		$scope.cancel = () => {
+			$scope.entity = {};
+			$scope.action = 'select';
+			Dialogs.closeWindow({ id: 'Dimension-details' });
+		};
+
+		$scope.clearErrorMessage = () => {
+			$scope.errorMessage = null;
+		};
+	});
