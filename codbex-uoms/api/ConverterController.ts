90f7c05da8acf69f691f3a57acd0cca1dffc2108
ThuF <jordan.p.pavlov@gmail.com>
2026-02-19 13:51:12 +0000

    Fix ConverterController

--- codbex-uoms/api/ConverterController.ts
+++ codbex-uoms/api/ConverterController.ts
@@ -1,32 +1,50 @@
-import { Controller, Get } from "sdk/http"
-import { UoMRepository } from "../gen/codbex-uoms/dao/Settings/UoMRepository";
-import { HttpUtils } from "../gen/codbex-uoms/api/utils/HttpUtils";
+import { Controller, Get, Documentation } from "@aerokit/sdk/http"
+import { Operator } from "@aerokit/sdk/db"
+import { UoMRepository } from "../gen/codbex-uoms/data/Settings/UoMRepository"
+import { HttpUtils } from "@aerokit/sdk/http/utils"
 
 /**
  * Converts Source UoM to Target UoM the given Value
- * Example: http://host:port/services/ts/codbex-uoms/api/converter.ts/CMT/DMT/50
+ * Example: http://host:port/services/ts/codbex-uoms/api/ConverterController.ts/KGM/GRM/50
  */
 
+@Documentation('codbex-uoms - Converter Controller')
 @Controller
-class UoMConverterService {
+class ConverterController {
 
     private readonly repository = new UoMRepository();
 
     @Get("/:source/:target/:value")
-    public convertValue(_: any, ctx: any) {
+    public convertValue(_: any, ctx: any): number {
         try {
             const source = ctx.pathParameters.source;
             const target = ctx.pathParameters.target;
             const value = parseFloat(ctx.pathParameters.value);
-            const entitySource = this.repository.findById(source);
-            const entityTarget = this.repository.findById(target);
+            const entitySource = this.repository.findAll({
+                conditions: [
+                    {
+                        operator: Operator.EQ,
+                        propertyName: 'ISO',
+                        value: source
+                    }
+                ]
+            })[0];
+            const entityTarget = this.repository.findAll({
+                conditions: [
+                    {
+                        operator: Operator.EQ,
+                        propertyName: 'ISO',
+                        value: target
+                    }
+                ]
+            })[0];
             if (entitySource && entityTarget) {
                 if (entitySource.Dimension !== entityTarget.Dimension) {
                     HttpUtils.sendResponseBadRequest("Both Source and Target Unit of Measures should have the same Dimension");
                     return;
                 }
-                let valueBase = value * entitySource.Numerator / entitySource.Denominator;
-                let valueTarget = valueBase * entityTarget.Denominator / entityTarget.Numerator;
+                const valueBase = value * entitySource.Numerator / entitySource.Denominator;
+                const valueTarget = valueBase * entityTarget.Denominator / entityTarget.Numerator;
                 return valueTarget;
             } else {
                 HttpUtils.sendResponseNotFound("Unit of Measures not found: [" + source + "] and/or [" + target + "]");
