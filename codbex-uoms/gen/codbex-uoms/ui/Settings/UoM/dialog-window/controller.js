59f8f97080916a8f9759fcea92dba4d3b874c1bd
tivmof <tomyivanov06@gmail.com>
2026-04-29 15:42:16 +0000

    chore: Regenerating module

--- codbex-uoms/gen/codbex-uoms/ui/Settings/UoM/dialog-window/controller.js
+++ codbex-uoms/gen/codbex-uoms/ui/Settings/UoM/dialog-window/controller.js
@@ -35,7 +35,9 @@ angular.module('page', ['blimpKit', 'platformView', 'platformLocale', 'EntitySer
 			$scope.entity = params.entity;
 			$scope.selectedMainEntityKey = params.selectedMainEntityKey;
 			$scope.selectedMainEntityId = params.selectedMainEntityId;
-			$scope.optionsDimension = params.optionsDimension;
+			const optionsDimensionMap = new Map();
+			params.optionsDimension?.forEach(e => optionsDimensionMap.set(e.value, e));
+			$scope.optionsDimension = Array.from(optionsDimensionMap.values());
 		}
 
 		$scope.create = () => {
@@ -97,6 +99,99 @@ angular.module('page', ['blimpKit', 'platformView', 'platformLocale', 'EntitySer
 				type: AlertTypes.Error
 			});
 		});
+
+		const lastSearchValuesDimension = new Set();
+		const allValuesDimension = [];
+		let loadMoreOptionsDimensionCounter = 0;
+		$scope.optionsDimensionLoading = false;
+		$scope.optionsDimensionHasMore = true;
+
+		$scope.loadMoreOptionsDimension = () => {
+			const limit = 20;
+			$scope.optionsDimensionLoading = true;
+			$http.get(`/services/ts/codbex-uoms/gen/codbex-uoms/api/Settings/DimensionController.ts?$limit=${limit}&$offset=${++loadMoreOptionsDimensionCounter * limit}`)
+			.then((response) => {
+				const optionValues = allValuesDimension.map(e => e.value);
+				const resultValues = response.data.map(e => ({
+					value: e.Id,
+					text: e.Name
+				}));
+				const newValues = [];
+				resultValues.forEach(e => {
+					if (!optionValues.includes(e.value)) {
+						allValuesDimension.push(e);
+						newValues.push(e);
+					}
+				});
+				newValues.forEach(e => {
+					if (!$scope.optionsDimension.find(o => o.value === e.value)) {
+						$scope.optionsDimension.push(e);
+					}
+				})
+				$scope.optionsDimensionHasMore = resultValues.length > 0;
+				$scope.optionsDimensionLoading = false;
+			}, (error) => {
+				$scope.optionsDimensionLoading = false;
+				console.error(error);
+				const message = error.data ? error.data.message : '';
+				Dialogs.showAlert({
+					title: 'Dimension',
+					message: LocaleService.t('codbex-uoms:codbex-uoms-model.messages.error.unableToLoad', { message: message }),
+					type: AlertTypes.Error
+				});
+			});
+		};
+
+		$scope.onOptionsDimensionChange = (event) => {
+			if (allValuesDimension.length === 0) {
+				allValuesDimension.push(...$scope.optionsDimension);
+			}
+			if (event.originalEvent.target.value === '') {
+				allValuesDimension.sort((a, b) => a.text.localeCompare(b.text));
+				$scope.optionsDimension = allValuesDimension;
+				$scope.optionsDimensionHasMore = true;
+			} else if (isText(event.which)) {
+				$scope.optionsDimensionHasMore = false;
+				let cacheHit = false;
+				Array.from(lastSearchValuesDimension).forEach(e => {
+					if (event.originalEvent.target.value.startsWith(e)) {
+						cacheHit = true;
+					}
+				})
+				if (!cacheHit) {
+					$http.post('/services/ts/codbex-uoms/gen/codbex-uoms/api/Settings/DimensionController.ts/search', {
+						conditions: [
+							{ propertyName: 'Name', operator: 'LIKE', value: `${event.originalEvent.target.value}%` }
+						]
+					}).then((response) => {
+						const optionValues = allValuesDimension.map(e => e.value);
+						const searchResult = response.data.map(e => ({
+							value: e.Id,
+							text: e.Name
+						}));
+						searchResult.forEach(e => {
+							if (!optionValues.includes(e.value)) {
+								allValuesDimension.push(e);
+							}
+						});
+						$scope.optionsDimension = allValuesDimension.filter(e => e.text.toLowerCase().startsWith(event.originalEvent.target.value.toLowerCase()));
+					}, (error) => {
+						console.error(error);
+						const message = error.data ? error.data.message : '';
+						Dialogs.showAlert({
+							title: 'Dimension',
+							message: LocaleService.t('codbex-uoms:codbex-uoms-model.messages.error.unableToLoad', { message: message }),
+							type: AlertTypes.Error
+						});
+					});
+					lastSearchValuesDimension.add(event.originalEvent.target.value);
+				}
+			}
+		};
+		function isText(keycode) {
+			if ((keycode >= 48 && keycode <= 90) || (keycode >= 96 && keycode <= 111) || (keycode >= 186 && keycode <= 222) || [8, 46, 173].includes(keycode)) return true;
+			return false;
+		}
 
 		$scope.$watch('entity.Dimension', (newValue, oldValue) => {
 			if (newValue !== undefined && newValue !== null) {
